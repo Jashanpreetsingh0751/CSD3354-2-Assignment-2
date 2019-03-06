6df17eafa3f1a7b86b9dac5ddf41495fde857341
visualstudio <visualstudio@122-PC32>
2019-03-06 10:35:27 -0500

    lab1

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Student Name Jashanpreet Singh Student ID C0732125
+//Student Name Vikramjot Singh Student ID C0726863
+//Assignment 2
+//March 6,2019
 
 namespace CSD3354_2_Assignment_2
 {
-    class Program
+    class proram
     {
         static void Main(string[] args)
         {
+            DelegateExercises a = new DelegateExercises();
+            a.Method2();
+        }
+    }
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
